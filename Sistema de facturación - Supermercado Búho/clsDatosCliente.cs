d61788afc84467d7b862b5033f0ca2987b58ff5a
CHRISTIAN-ARAGON00 <christian.aragon@epn.edu.ec>
2022-02-22 16:45:11 -0500

    Diseño y Corrección de Errores.

---
+++ Sistema de facturación - Supermercado Búho/clsDatosCliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_facturación___Supermercado_Búho
+{
+    public class clsDatosCliente
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string NumeroDeTelefono { get; set; }
+
+    }
+}
---
+++ Sistema de facturación - Supermercado Búho/clsProducto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_facturación___Supermercado_Búho
+{
+    internal class clsProducto
+    {
+        public string Nombre { get; set; }
+        public string Cantidad { get; set; }
+        public string PrecioSinIva { get; set; }
+        public string PrecioFinal { get; set; }
+    }
+}
